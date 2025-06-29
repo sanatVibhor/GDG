84752681c9e6267477f1ed4493a679da589ada6f
sanatVibhor <sanatvibhor2@gmail.com>
2025-06-29 13:43:53 +0530

    sanat react file initial commit

---
+++ mainWorkingFolder/saturday28th/components/JSX.jsx
@@ -0,0 +1,4 @@
+export default function JSX() {
+    const userName = "GDG Ranchi";
+    return <h1>Hello, {userName}!</h1>;
+  }
---
+++ mainWorkingFolder/saturday28th/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import JSX from '../components/JSX'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <JSX/>
+    </>
+  )
+}
+
+export default App
